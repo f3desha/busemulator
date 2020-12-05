55708189bcaf70fb6fe21066e96c0e5243f529f3
Alexander Kuzmenko <kuzmenko@singree.com>
2020-12-05 17:22:18 +0200

    added plan for api endpoints

--- routes/api.php
+++ routes/api.php
@@ -13,16 +13,27 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::group(['middleware' => 'role:admin'], function() {
+    //Browse all routes
+    //Read a single route
+    //Edit a single route
+    //Add a new route
+    //Delete route
+    //Add route points
+    //Delete route points
 
+    //Browse all buses
+    //Read a single bus information
+    //Edit a single bus information
+    //Add a new bus
+    //Delete bus
+
+    //Browse all bus drivers
+    //Read a single bus driver info
+    //Set regular user as a Bus driver role (ADD)
+    //Remove bus driver role from a user (DELETE)
 });
 
 Route::group(['middleware' => 'role:driver'], function() {
-    Route::get('/go', function (){
-        return 1234;
-    });
+    //Drive a bus to the next route point
 });
